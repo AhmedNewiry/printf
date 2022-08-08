43fa18a8b24d6ccc85e6aab5dd27a5f203078d70
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-08 14:47:06 -0700

    printf header

--- printf.c
+++ printf.c
@@ -5,3 +5,13 @@ int _printf(const char *format, ...);
  * _puts-prints a string
  * @str: pointer to the first char of the string
  */
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+}
