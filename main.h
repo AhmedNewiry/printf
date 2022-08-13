49b459774a2ebb920bc471d810433d88dc0a1844
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-08-13 11:18:17 -0700

    main

--- main.h
+++ main.h
@@ -2,6 +2,24 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+/**
+ * struct spec_controller- checker constructor
+ * @c: the data type
+ * @f_ptr: pointer to the printer fucntion
+ */
+typedef struct spec_controller
+{
+char c;
+int (*f_ptr)(va_list);
+} spec_controller;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_str(va_list ptr);
+int print_c(va_list ch);
+int print_int(va_list ptr);
+char *_itoa(long int num, int base);
+int print_binary(va_list ptr);
+int _strlen(const char *s);
 #endif
