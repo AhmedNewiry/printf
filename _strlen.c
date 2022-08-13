d451dd2d9f386135248a65de4cae19fcaee59c28
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-08-13 11:17:04 -0700

    _strlen

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen- a function that finds string length
+ * @s: the pointer to the string
+ * Return: the string length
+ */
+
+int _strlen(const char *s)
+{
+int i = 0;
+while (s[i])
+{
+i++;
+}
+return (i);
+}
